bbe40c6b4201a3d52f482eeee731aa8e25ed287d
Le Kos <mateo.kos100@gmail.com>
2023-12-02 13:59:19 +0100

    Podjelio InputParsing na logicke cjeline

---
+++ Kalkulator/ChangeColor.cs
@@ -0,0 +1,68 @@
+﻿namespace InputParsing;
+
+
+public static class ChangeColor
+{
+    public static void WhiteTX(string s)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write(s);
+        Console.ResetColor();
+    }
+
+    public static void GrayTX(string s)
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write(s);
+        Console.ResetColor();
+    }
+
+    public static void RedBG(string s)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.DarkRed;
+        Console.Write(s);
+        Console.ResetColor();
+    }
+
+    public static void RedTX(string s)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write(s);
+        Console.ResetColor();
+    }
+
+    public static void GreenBG(string s)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.DarkGreen;
+        Console.Write(s);
+        Console.ResetColor();
+    }
+
+    public static string YellowTX(string s)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write(s);
+        Console.ResetColor();
+        return "";
+    }
+
+    public static string GreenBGLine(string s)
+    {
+        GreenBG(s);
+        Console.WriteLine();
+        return "";
+    }
+
+    public static string RedBGLine(string s)
+    {
+        RedBG(s);
+        Console.WriteLine();
+        return "";
+    }
+}
---
+++ Kalkulator/CommandColor.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputParsing;
+
+public static class CommandColor
+{
+    public static string LineColoring(string userInput, List<Varijabla> Varijable)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("                                \r"); //Da očisti help poruku
+        if (userInput == "") Console.Write("Upiši \"?\" za pomoc\r");
+
+        (int x_pomak, int y_pomak) = Console.GetCursorPosition();
+        x_pomak = Console.CursorLeft;
+        Console.SetCursorPosition(x_pomak, y_pomak - 1);
+
+        if (userInput == "") return userInput;
+
+        Console.Write(">");
+
+        //Console.Write("\x1b[1A\r\x1b[2K\r>");
+
+        List<BojaOperacija> userInputOperandi = new List<BojaOperacija> { AddOperacija(0, "0") };
+        userInputOperandi.RemoveAt(0);
+        if (userInputOperandi.Count > 0) throw new InvalidProgramException("Prazno polje operanada nije prazno!");
+
+        string tempInput = "";
+        int brojOperanada = 0;
+
+
+        for (int i = 0; i < userInput.Length; i++)
+        {
+            tempInput += userInput[i];
+
+            if (tempInput == "+" || tempInput == "-" || tempInput == "*" || tempInput == "/" || tempInput == "^" || tempInput == "!" || tempInput == "=")
+            {
+                userInputOperandi.Add(AddOperacija((int)ConsoleColor.White, tempInput));
+                brojOperanada++;
+                tempInput = "";
+                continue;
+            }
+
+            if (tempInput == " ")
+            {
+                tempInput = "";
+                continue;
+            }
+
+            if (userInput[(i + 1) % userInput.Length] == ' ' || i == userInput.Length - 1 || char.IsAscii(userInput[(i + 1) % userInput.Length])
+                && !(char.IsLetterOrDigit(userInput[(i + 1) % userInput.Length]) || userInput[(i + 1) % userInput.Length] == '.'))
+            {
+                //Ako je ključna riječ -> obojaj u ljubičasto
+                if (tempInput.ToUpper() == UserInputParser.defineVariable || tempInput.ToUpper() == UserInputParser.helpMeni ||
+                    tempInput.ToUpper() == UserInputParser.exitFromCalc || tempInput.ToUpper() == UserInputParser.clScr ||
+                    tempInput == "?" || tempInput.ToUpper() == "ESC")
+                {
+                    userInputOperandi.Add(AddOperacija((int)ConsoleColor.Magenta, tempInput));
+                }
+                else
+                {
+                    //Ako varijabla ne postoji, ovisi o mjestu u komandi obojaj u zeleno(spremanje u novu) ili crveno(računanje s novom)
+                    var tempVar = Varijable.FindPerName(tempInput);
+                    if (tempVar == null || UserInputParser.IsNumber(tempInput))
+                        if (UserInputParser.IsNumber(tempInput))
+                            //Ako je broj -> obojaj u cyane
+                            userInputOperandi.Add(AddOperacija((int)ConsoleColor.Cyan, tempInput));
+                        else if (tempInput == userInput.Split("=")[0].Split(" ")[0])
+                            userInputOperandi.Add(AddOperacija((int)ConsoleColor.Green, tempInput));
+                        else
+                            userInputOperandi.Add(AddOperacija((int)ConsoleColor.Red, tempInput));
+                    else
+                        //Ime postojece matrice obojaj u žutu
+                        userInputOperandi.Add(AddOperacija((int)ConsoleColor.Yellow, tempInput));
+                }
+
+                brojOperanada++;
+                tempInput = "";
+            }
+
+        }
+
+        List<BojaOperacija> outputString = new List<BojaOperacija> { AddOperacija(0, "0") };
+        outputString.RemoveAt(0);
+
+        for (int i = 0; i < userInputOperandi.Count; i++)
+        {
+            if (userInputOperandi[(i + 1) % userInputOperandi.Count].operacija.Contains('^')
+                || userInputOperandi[i].operacija.Contains('^')
+                || userInputOperandi[(i + 1) % userInputOperandi.Count].operacija.Contains('!')
+                || userInputOperandi[i].operacija.Contains('!')
+                || i == userInputOperandi.Count - 1)
+                outputString.Add(userInputOperandi[i]);
+            else
+            {
+                outputString.Add(userInputOperandi[i]);
+                outputString[i] = AddOperacija(outputString[i].boja, outputString[i].operacija + " ");
+            }
+        }
+
+        for (int i = 0; i < outputString.Count; i++)
+        {
+            Console.ForegroundColor = (ConsoleColor)outputString[i].boja;
+            Console.Write(outputString[i].operacija);
+        }
+
+        int brojZnakovaOperanada = 0;
+        for (int i = 0; i < outputString.Count; i++)
+            brojZnakovaOperanada += outputString[i].operacija.Length;
+
+        Console.WriteLine(string.Empty.PadRight(Math.Abs(userInput.Length - brojZnakovaOperanada)));
+
+        string outputStringUnEscaped = "";
+        for (int i = 0; i < outputString.Count; i++)
+        {
+            outputStringUnEscaped += outputString[i].operacija;
+        }
+
+        return outputStringUnEscaped;
+
+    }
+
+    private static BojaOperacija AddOperacija(int bojaOperacije, string tekstOperacije)
+    {
+        BojaOperacija bojaOperacija;
+        bojaOperacija.boja = bojaOperacije;
+        bojaOperacija.operacija = tekstOperacije;
+
+        return bojaOperacija;
+    }
+
+    private struct BojaOperacija
+    {
+        public int boja;
+        public string operacija;
+    }
+}
